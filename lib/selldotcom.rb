af700f17c727aaa318855d368051ec6c38989181
Ezekiel Templin <ezkl@me.com>
2011-04-14 17:52:49 -0400

    And I am an asshole.

--- lib/selldotcom.rb
+++ lib/selldotcom.rb
@@ -2,9 +2,9 @@ require 'typhoeus'
 require 'nokogiri'
 require 'time'
 
-relative_require 'selldotcom/version'
-relative_require 'selldotcom/search'
-relative_require 'selldotcom/page'
+require_relative 'selldotcom/version'
+require_relative 'selldotcom/search'
+require_relative 'selldotcom/page'
 
 module SellDotCom
   def self.is_active?(url)
